69d4fd93e87ecf7dad91caa2c2934cca3c82f685
Harold_Mesa <harold.mesa@icloud.com>
2022-10-31 13:38:45 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2nd_Team_Week_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
